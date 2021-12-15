a86e584eb7f86436cdb1cb9deee7282b12aebe29
yanglbme <szuyanglb@outlook.com>
2021-12-15 16:37:09 +0800

    test: github context

--- main.py
+++ main.py
@@ -2,12 +2,17 @@ import requests
 
 from actions_toolkit import core
 
+from actions_toolkit.context import Context
+
 
 def main():
     requests.get('https://httpbin.org/get?code=0')
     username = core.get_input('username', required=True)
     res = f"Hello {username}"
-    core.set_output(res)
+    core.set_output('res', res)
+    context = Context()
+    print(context)
+    print(context.payload)
 
 
 if __name__ == "__main__":
