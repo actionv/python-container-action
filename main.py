9d3feeefbb6f3a16b8e873d62fb77a1532e6c401
yanglbme <szuyanglb@outlook.com>
2021-12-15 20:08:35 +0800

    chore: update action

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
+from datetime import datetime, timezone, timedelta
+
 import requests
-
 from actions_toolkit import core
-
 from actions_toolkit.context import Context
 
 
 def main():
     requests.get('https://httpbin.org/get?code=0')
-    username = core.get_input('username', required=True)
+    username = core.get_input('username')
     res = f"Hello {username}"
     core.set_output('res', res)
+    tz = timezone(timedelta(hours=+8))
+    time = datetime.now(tz).strftime('%Y-%m-%d %H:%M:%S')
+    print(f"::set-output name=time::{time}")
     context = Context()
     print(context)
     print(context.payload)
